1da6149c60c9151783b072c947893023cb988dbe
EM-H20 <78837018+EM-H20@users.noreply.github.com>
2026-06-27 23:46:51 +0900

    fix : Android core library desugaring 활성화
    - flutter_local_notifications가 요구하는 desugaring 미설정으로 Android 빌드 실패 해결
    - isCoreLibraryDesugaringEnabled 활성화 및 desugar_jdk_libs 2.1.4 의존성 추가

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -11,6 +11,8 @@ android {
     ndkVersion = flutter.ndkVersion
 
     compileOptions {
+        // flutter_local_notifications 요구: core library desugaring 활성화
+        isCoreLibraryDesugaringEnabled = true
         sourceCompatibility = JavaVersion.VERSION_11
         targetCompatibility = JavaVersion.VERSION_11
     }
@@ -41,4 +43,8 @@ android {
 
 flutter {
     source = "../.."
+}
+
+dependencies {
+    coreLibraryDesugaring("com.android.tools:desugar_jdk_libs:2.1.4")
 }
